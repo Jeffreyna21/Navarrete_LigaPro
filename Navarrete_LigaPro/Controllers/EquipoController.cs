eb4929f5581bb08086c32cbf76f249a843cfaf66
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-04-13 20:10:43 -0500

    Primeros cambios

---
+++ Navarrete_LigaPro/Controllers/EquipoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Navarrete_LigaPro.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Navarrete_LigaPro/Models/Equipo.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Navarrete_LigaPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre el equipo")]
+
+        public string Nombre { get; set; }
+        [Range(0, 100)]
+        public int partidosJugados { get; set; }
+        [Range(0, 100)]
+
+        public int partidosGanados { get; set; }
+        [Range(0, 100)]
+
+        public int partidosEmpatados { get; set; }
+        [Range(0, 100)]
+        public int partidosPerdidos { get; set; }
+
+
+    }
+}
