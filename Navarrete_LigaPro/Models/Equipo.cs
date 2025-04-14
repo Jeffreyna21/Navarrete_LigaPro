fe1f9b44441209eecb669e1e2ede646db6e1f2f0
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-04-13 21:10:53 -0500

    agreamos el carrusel y calculo de puntos

--- Navarrete_LigaPro/Models/Equipo.cs
+++ Navarrete_LigaPro/Models/Equipo.cs
@@ -23,6 +23,8 @@ namespace Navarrete_LigaPro.Models
         [Range(0, 100)]
         public int partidosPerdidos { get; set; }
 
+        public int Puntos => (partidosGanados * 3) + (partidosEmpatados * 1);
+
 
     }
 }
